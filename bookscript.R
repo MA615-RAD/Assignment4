0259c274bc03068232eb1de086174cb5717a25c2
radonahue <radona@bu.edu>
2021-12-07 20:54:52 -0500

    commit updats

--- bookscript.R
+++ bookscript.R
@@ -6,21 +6,25 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(wordcloud)
+library(sqldf)
 
 gatsby<-gutenberg_download(c(64317))
-
-
+gatsby$text<ifelse(gatsby$text=="I", "CHAPTER 1", )
 
 tidy_gatsby <- gatsby %>%
   unnest_tokens(word, text) %>%
   anti_join(stop_words)
+
+r<-as.character(as.roman(1:9))
+
+gatsby$t<-ifelse(str_detect(gatsby$text, r), 1, 0)
+sqldf("select case when text like I then 'Chapter 1' end from gatsby where text in")
 
 #word count
 tidy_gatsby %>%
   count(word, sort = TRUE)
 
 get_sentiments("bing")
-
 
 bing_positive <- get_sentiments("bing") %>% 
   filter(sentiment == "positive")
@@ -108,5 +112,28 @@ tidy_g %>%
 library(sentimentr)
 sentiment(gatsby)
 #https://www.r-bloggers.com/2020/04/sentiment-analysis-in-r-with-sentimentr-that-handles-negation-valence-shifters/
+
+
+library(tnum)
+tnum.authorize("mssp1.bu.edu")
+tnum.getDBPathList(taxonomy="subject", levels=1)
+
+tnum.setSpace("test2")
+
+source("Book2TN-v6A-1.R")
+
+tnBooksFromLines(gatsby$text, "gatsby/hw_1")
+?tnBooksFromLines
+
+tnum.getDBPathList(taxonomy="subject", levels=2)
+q<-tnum.query(query = "gatsby/hw_1# has *", max=23112)
+df<-tnum.objectsToDf(q)
+df1<-df%>%filter(property=="text")
+
+library(sentimentr)
+
+sentiment=sentiment_by(df1$string.value)
+
+ggplot(data=sentiment, aes(x=element_id, y=ave_sentiment))+geom_point()
 
 
